c1d204f72d44f888dc60d905bd55411fd5145080
Luca Suriano <luca.suriano@wetransfer.com>
2020-01-15 12:34:25 +0100

    Adding benchmark spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'rspec'
 require 'wt_s3_signer'
 require 'aws-sdk-s3'
+require 'rspec-benchmark'
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 $LOAD_PATH.unshift(File.dirname(__FILE__))
@@ -11,5 +12,6 @@ AWS_ALLOCATOR = ResourceAllocator.new
 
 RSpec.configure do |config|
   config.order = 'random'
+  config.include RSpec::Benchmark::Matchers
   AWS_ALLOCATOR.install_rspec_hooks!(config)
 end
--- spec/url_signing_spec.rb
+++ spec/url_signing_spec.rb
@@ -19,6 +19,15 @@ end
 describe WT::S3Signer do
   include_context 'signer_bucket'
 
+  it 'WT::Signer is faster than Aws::S3::Presigner' do
+    allow(WT::S3Signer).to receive(:create_bucket).and_return(bucket)
+
+    bucket.object('dir/testobject').put(body: 'is here')
+
+    expect { bucket.object('dir/testobject').presigned_url(:get, expires_in: 173) }.to perform_at_least(1000).ips
+    expect { signer.presigned_get_url(object_key: 'dir/testobject') }.to perform_at_least(100000).within(0.4).warmup(0.2).ips
+  end
+
   it 'signs an s3 key' do
     allow(WT::S3Signer).to receive(:create_bucket).and_return(bucket)
 
@@ -26,7 +35,6 @@ describe WT::S3Signer do
     presigned_url = signer.presigned_get_url(object_key: 'dir/testobject')
 
     expect(presigned_url).to include("X-Amz-Expires=173")
-
   end
 
   it 'signs a valid s3 key' do
@@ -39,10 +47,10 @@ describe WT::S3Signer do
     res = Net::HTTP.get_response(uri)
 
     expect(res.code).to eq("200")
-
   end
 
   it 'throws an exception if no key is used for signing' do 
     expect{signer.presigned_get_url(object_key: '')}.to raise_error(ArgumentError)
   end
+
 end
